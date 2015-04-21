566a54401793e19691871e07b0a8d7294f451f5a
d14lubra <d14lubra@WIN-20M8BI1GG7E>
2015-04-21 10:18:15 +0200

    Añadido mas pruebas con if

--- PythonDev/PythonTesting/if.py
+++ PythonDev/PythonTesting/if.py
@@ -2,3 +2,10 @@ edad = 18
 if edad >= 18:
     print "x es positivo"
     print "debes ser responsable"
+#Si esta indentado lo reconoce como que esta dentro del if, de no ser asi creera que pertenece a otra cosa.
+#Entrada estandar de datos
+entrada=int(raw_input('Dame un numero: '))
+print entrada
+cad=raw_input('dame una cadena: ')
+print cad
+real= float(raw_input())
